20e89ca6c6c48bd7a2a1da69457e3126d61576aa
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-04 17:12:33 +0100

    usertaskcontroller

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::resource('task', 'Taskscontroller');
-    Route::resource('user', 'Userscontroller');
+    Route::resource('user', 'UsersController');
+    Route::resource('user.task', 'UserTaskscontroller');
+    //Route::resource('task.user', 'TaskUserscontroller');
     });
-
-
-//Route::resource('task', 'Taskscontroller');
-//Route::resource('user', 'Userscontroller');
