6296e133fbaa19914f17c60c82d294675af8b8f0
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-22 20:52:52 +0100

    passport api_token v2

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -27,6 +29,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes();
 
         //
     }
