280f8624da8c0d8d5feec39da2d91f3efce87713
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-22 20:47:20 +0100

    auth web v3 & passport install

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Passport\PassportServiceProvider::class,
+
         //
 
         /*
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 
-Route::group(['prefix' => 'v1', 'middleware' => ['auth.basic']], function () {
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
     Route::resource('task', 'Taskscontroller');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTaskController');
