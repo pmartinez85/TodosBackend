bf28036ad9e7d56762db95a18a80b53a9b2704af
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-30 19:49:10 +0100

    paginationrange v1

--- resources/assets/js/components/Pagination.vue
+++ resources/assets/js/components/Pagination.vue
@@ -1,9 +1,12 @@
 <template>
         <ul class="pagination pagination-sm no-margin pull-right">
-            <li><a href="#">&laquo;</a></li>
+            <li v-for="n in paginationRange">
+                <a href="#">&laquo;</a></li>
+
             <li><a href="#">1</a></li>
             <li><a href="#">2</a></li>
             <li><a href="#">3</a></li>
+
             <li><a href="#">&raquo;</a></li>
         </ul>
 </template>
